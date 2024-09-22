a7b71de4e76ed861e391832de1ccce2897f92cd4
Alecsander Moraes <alecsandermoraesdossantos@gmail.com>
2024-09-22 15:26:00 -0300

    New Initial Template for app.js

--- app.js
+++ app.js
@@ -1,29 +1,8 @@
 const { app, BrowserWindow } = require('electron');
-const path = require('node:path');
+let mainWindow; 
 
-const createWindow = () => {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.js'),
-            nodeIntegration: true
-        }
-    });
-
-    win.loadFile('index.html');
-}
-
-app.whenReady().then(createWindow);
-
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({});
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
 
-app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-    }
-});
