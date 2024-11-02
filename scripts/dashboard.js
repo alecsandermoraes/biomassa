4b0b2ca5b025e1523066c3277595f56addc67d95
Dark <alecsandermoraesdossantos@gmail.com>
2024-11-02 17:30:20 -0300

    Gráfico da Matriz Energética Brasileira de 2023

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -25,3 +25,78 @@ const productivityChart = new Chart(ctx, {
         responsive: true
     }
 });
+
+const ctx2 = document.getElementById('energyPieChart').getContext('2d');
+const energyPieChart = new Chart(ctx2, {
+    type: 'pie',
+    data: {
+        labels: [
+            'Petróleo e Derivados (35,7%)',
+            'Biomassa da Cana (15,4%)',
+            'Hidráulica (12,5%)',
+            'Gás Natural (10,5%)',
+            'Lenha e Carvão Vegetal (9%)',
+            'Lixívia e Outras Renováveis (7%)',
+            'Carvão Mineral (4,6%)',
+            'Eólica (2,3%)',
+            'Nuclear (1,3%)',
+            'Solar (1,2%)',
+            'Não Renováveis (0,6%)',
+        ],
+
+        datasets: [{
+            label: 'Uso de Energia (%)',
+            data: [35.7, 15.4, 12.5, 10.5, 9.0, 7.0, 4.6, 2.3, 1.3, 1.2, 0.6],
+            backgroundColor: [
+                '#4CAF50',
+                '#66BB6A',
+                '#81C784',
+                '#A5D6A7',
+                '#C8E6C9',
+                '#388E3C',
+                '#1B5E20',
+                '#AEEEEE',
+                '#B2DFDB',
+                '#7CB342',
+                '#9CCC65',
+            ],
+
+            borderWidth: 1,
+            borderColor: '#000000',
+        }]
+    },
+
+    options: {
+        responsive: true,
+        layout: {
+            padding: {
+                top: 20,
+                right: 20,
+                bottom: 20,
+                left: 20,
+            },
+        },
+
+        plugins: {
+            legend: {
+                display: true,
+                position: 'right',
+                labels: {
+                    boxWidth: 25,
+                    padding: 20,
+                    font: {
+                        size: 14,
+                    }
+                }
+            },
+
+            tooltip: {
+                callbacks: {
+                    label: function(tooltipItem) {
+                        return `${tooltipItem.label}: ${tooltipItem.raw.toFixed(1)}%`;
+                    }
+                }
+            }
+        }
+    }
+});
